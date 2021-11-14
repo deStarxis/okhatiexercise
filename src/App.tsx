d805534faadecc77bad24951994ee054de9bfc30
Starcly56 <humagainuzjwol@gmail.com>
2021-11-14 15:20:16 +0545

    included authentication with all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
 
 function App() {
+  const loginToken = localStorage.getItem("loginToken");
+  const submittedEmail = localStorage.getItem("submittedEmail");
+  const submittedPassword = localStorage.getItem("submittedPassword");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Happy coding!!!
-        </p>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          {!loginToken && !submittedEmail && !submittedPassword && (
+            <>
+              <Route exact path="/register" component={Register} />
+              <Redirect to="/register" />
+            </>
+          )}
+          {!loginToken && (
+            <>
+              <Route exact path="/login" component={Login} />
+              <Redirect to="/login" />
+            </>
+          )}
+          {loginToken && (
+            <>
+              <Route exact path="/" component={Home} />
+              <Redirect to="/" />
+            </>
+          )}
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
