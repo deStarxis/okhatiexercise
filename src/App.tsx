aa9c9a211c6edd08d8859db8bd9af0b43b1a55d3
Starcly56 <humagainuzjwol@gmail.com>
2021-11-14 19:18:21 +0545

    included new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,18 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Home from "./components/Home";
+import { BrowserRouter } from "react-router-dom";
+
+import Body from "./components/Body";
+import Header from "./components/Header";
 
 function App() {
-  const loginToken = localStorage.getItem("loginToken");
-  const submittedEmail = localStorage.getItem("submittedEmail");
-  const submittedPassword = localStorage.getItem("submittedPassword");
-
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          {!loginToken && !submittedEmail && !submittedPassword && (
-            <>
-              <Route exact path="/register" component={Register} />
-              <Redirect to="/register" />
-            </>
-          )}
-          {!loginToken && (
-            <>
-              <Route exact path="/login" component={Login} />
-              <Redirect to="/login" />
-            </>
-          )}
-          {loginToken && (
-            <>
-              <Route exact path="/" component={Home} />
-              <Redirect to="/" />
-            </>
-          )}
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Body />
+      </div>
+    </BrowserRouter>
   );
 }
 
